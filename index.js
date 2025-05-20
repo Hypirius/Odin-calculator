fc824efcf4f4d041a53feb415866eeefd681b686
Hypirius <zuhayerhossain25@gmail.com>
2025-05-20 20:38:09 +0600

    Add negation functionality and siderbar event

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const numpad = document.querySelector(".numpad");
+const sideNumpad = document.querySelector(".side-numpad");
 const display = document.querySelector(".display");
 
 numpad.addEventListener("click", function (event) {
@@ -11,8 +12,14 @@ numpad.addEventListener("click", function (event) {
     removeAllDisplay();
   } else if (clickClasslist.contains("control-buttons-BK")) {
     removeDisplayElement();
+  } else if (clickClasslist.contains("numpad-plus-minus")) {
+    valueNegator();
   }
 });
+
+sideNumpad.addEventListener("click", function (event) {
+
+})
 
 function addDisplay(keyValue) {
   display.append(keyValue);
@@ -25,4 +32,21 @@ function removeAllDisplay() {
 function removeDisplayElement() {
   displayChildren = display.childNodes;
   display.removeChild(displayChildren[displayChildren.length - 1]);
+}
+
+function toggleNegation(signValue) {
+  const sign = document.createTextNode(signValue);
+  display.insertBefore(sign, display.childNodes[0]);
+}
+
+function valueNegator() {
+  try {
+    if (display.childNodes[0].textContent === "-") {
+      display.removeChild(display.childNodes[0]);
+    } else {
+      toggleNegation("-");
+    }
+  } catch {
+    console.log("No value to negate");
+  }
 }
